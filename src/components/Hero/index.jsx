fd62eca3f39bd1881e3c3d2945165f7081af9f5f
Mibin Thomas <mibinthomas22@gmail.com>
2026-01-29 11:50:54 +0530

    hero section video bg added

--- src/components/Hero/index.jsx
+++ src/components/Hero/index.jsx
@@ -1,62 +1,56 @@
-import Button from '../Button';
-import parse from 'html-react-parser';
-import WaterWave from 'react-water-wave';
+import React from 'react';
+import { Link } from 'react-router-dom';
+import parser from 'html-react-parser';
+import SocialBtns from '../SocialBtns';
+import ReactPlayer from 'react-player';
+import WaterWave from 'react-water-wave'
 
-export default function HeroStyle2({ titleUp, title, btnText, btnUrl, bgUrl }) {
+export default function HeroStyle5({ bgImg, title, subTitle, srcUrl }) {
   return (
-    <WaterWave
-      className="cs_hero cs_style_2 d-flex align-items-center justify-content-center background-filled position-relative overflow-hidden cs_ripple_version"
-      imageUrl={bgUrl}
+    <section
+      className="cs_hero cs_style_4 bg-primary position-relative d-flex align-items-center justify-content-center overflow-hidden youtube-bg"
+      style={{ backgroundImage: `url('${bgImg}')` }}
     >
-      {() => (
-        <>
-          <div className="container">
-            <div className="cs_hero_text position-relative cs_zindex_3 text-center">
-              <h2 className="text-white cs_mb_15 fw-normal cs_fs_18">
-                {titleUp}
-              </h2>
-              <h1 className="text-white cs_mb_45 cs_fs_60 cs_fs_lg_46 text-uppercase">
-                {parse(title)}
-              </h1>
-              <Button btnText={btnText} btnUrl={btnUrl} />
-            </div>
-          </div>
-          <div className="bg-primary opacity-75 position-absolute w-100 h-100 start-0 top-0" />
-          <div className="cs_hero_shape_1 position-absolute bottom-0 start-0 d-flex align-items-end h-100">
-            <svg
-              width={660}
-              height={759}
-              viewBox="0 0 660 759"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
+      <ReactPlayer
+        url="https://www.youtube.com/watch?v=I2SeBj6R-mc"
+        width="100%"
+        height="100%"
+        controls={false}
+        loop={true}
+        playing={true}
+        muted={true}
+        playIcon={false}
+        className="cs_video_bg"
+      />
+      <div className="container position-relative cs_zindex_2">
+        <div className="cs_hero_text">
+          {title && (
+            <h1 className="text-white cs_mb_28 cs_fs_60 cs_fs_lg_46">
+              {' '}
+              {parser(title)}
+            </h1>
+          )}
+
+          {subTitle && (
+            <p className="text-white cs_mb_40">{parser(subTitle)}</p>
+          )}
+          <div className="cs_hero_btn">
+            <Link
+              to={srcUrl}
+              className="cs_btn cs_style_1 cs_fs_16 cs_rounded_5 cs_pl_30 cs_pr_30 cs_pt_10 cs_pb_10 overflow-hidden"
             >
-              <path
-                d="M240 0H660L430 759H0L240 0Z"
-                fill="url(#paint0_linear_81_1981)"
-              />
-              <defs>
-                <linearGradient
-                  id="paint0_linear_81_1981"
-                  x1={330}
-                  y1="119.5"
-                  x2="434.5"
-                  y2="1186.5"
-                  gradientUnits="userSpaceOnUse"
-                >
-                  <stop stopColor="white" stopOpacity={0} offset="none" />
-                  <stop
-                    offset="0.9999"
-                    stopColor="#D9D9D9"
-                    stopOpacity="0.35"
-                  />
-                  <stop offset={1} stopColor="#222121" stopOpacity={0} />
-                  <stop offset={1} stopColor="#222121" stopOpacity={0} />
-                </linearGradient>
-              </defs>
-            </svg>
+              <span>Get A Quote</span>
+            </Link>
+            <Link
+              to={srcUrl}
+              className="cs_btn cs_style_2 cs_fs_16 cs_rounded_5 cs_pl_30 cs_pr_30 cs_pt_9 cs_pb_9 overflow-hidden"
+            >
+              <span>Learn More</span>
+            </Link>
           </div>
-        </>
-      )}
-    </WaterWave>
+        </div>
+      </div>
+      <SocialBtns variant="cs_social_btns d-flex flex-wrap cs_column_gap_15 cs_row_gap_15 cs_transition_5" />
+    </section>
   );
 }
